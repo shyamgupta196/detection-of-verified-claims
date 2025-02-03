50c750672fdd1318d707326d38866e3e8a58fd7b
El Boukili <Ali.ElBoukili@gesis.org>
2025-02-03 17:47:26 +0100

    delete unused files

--- evaluation/__init__.py
+++
@@ -1,9 +0,0 @@
-from os.path import dirname, realpath
-
-filepath = realpath(__file__)
-
-dir_of_file = dirname(filepath)
-parent_dir_of_file = dirname(dir_of_file)
-
-
-DATA_PATH = parent_dir_of_file+"/data/"
